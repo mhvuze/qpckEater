e4f15cbf8bd90dc73faa0044be196e08fac8e5eb
mhvuze <vuze.mhf2resg@gmail.com>
2016-08-30 10:58:43 +0200

    Add unpacking feature

--- qpckEater/Program.cs
+++ qpckEater/Program.cs
@@ -13,37 +13,63 @@ namespace qpckEater
         {
             // Variables
             string mode = "";
-            string qpck_folder = "";
+            string qpck_file = "";
 
             // Print header
             Console.WriteLine("qpckEater by MHVuze");
             Console.WriteLine("=========================");
 
             // Handle arguments
-            if (args.Length < 2) { Console.WriteLine("ERROR: Not enough arguments specified.\nExtract: qpck -x <qpck_folder>"); return; }
+            if (args.Length < 2) { Console.WriteLine("ERROR: Not enough arguments specified.\nExtract: qpck -x <qpck>"); return; }
             mode = args[0];
-            qpck_folder = args[1];
+            qpck_file = args[1];
 
             // Check mode
             if (mode != "-x" && mode != "-c") { Console.WriteLine("ERROR: Unsupported mode specified."); return; }
 
             // Check file
-            if (Directory.Exists(qpck_folder) == false) { Console.WriteLine("ERROR: Specified qpck folder doesn't exist."); return; }
+            if (File.Exists(qpck_file) == false) { Console.WriteLine("ERROR: Specified qpck file doesn't exist."); return; }
 
             // Unpacking
             if (mode == "-x")
             {
-                // Iterate directory for required qpck's
-                string[] qpck_files = Directory.GetFiles(qpck_folder, "*.qpck").Select(file => Path.GetFileName(file)).ToArray();
-                Console.WriteLine("Found the following files:");
-                foreach (string file in qpck_files) { Console.WriteLine(file); };
-                Console.WriteLine("=========================");
+                // Variables
+                int magic = 0x37402858;
+                int count = 0;
+                long start_offset = 0;
 
-                if (!qpck_files.Any("conf.qpck".Contains)) { Console.WriteLine("ERROR: conf.qpck is missing."); return; };
-                if (!qpck_files.Any("data.qpck".Contains)) { Console.WriteLine("ERROR: data.qpck is missing."); return; };
-                if (!qpck_files.Any("bin.qpck".Contains)) { Console.WriteLine("ERROR: bin.qpck is missing."); return; };
+                using (BinaryReader reader = new BinaryReader(File.Open(qpck_file, FileMode.Open)))
+                {
+                    if (reader.ReadInt32() != magic) { Console.WriteLine("ERROR: Invalid header."); return; }
+                    count = reader.ReadInt32();
 
-                //
+                    // Create output directory
+                    string folder_path = Path.GetDirectoryName(qpck_file) + "\\" + Path.GetFileNameWithoutExtension(qpck_file);
+                    if (!Directory.Exists(folder_path)) { Directory.CreateDirectory(folder_path); }
+
+                    Console.WriteLine("File index:");
+                    for (int i = 0; i < count; i++)
+                    {
+                        // Get file info
+                        long offset = reader.ReadInt64();
+                        long hash = reader.ReadInt64();
+                        int size = reader.ReadInt32();
+                        Console.WriteLine(String.Format("#{0}, File {1} @ 0x{2}, Size {3} bytes",
+                            (i+1).ToString("D8"),
+                            hash.ToString("X16"),
+                            offset.ToString("X16"),
+                            size));
+
+                        start_offset = reader.BaseStream.Position;
+
+                        // Extract file
+                        reader.BaseStream.Seek(offset, SeekOrigin.Begin);
+                        byte[] file_bytes = reader.ReadBytes(size);
+                        File.WriteAllBytes(folder_path + "\\" + hash.ToString("X16"), file_bytes);
+
+                        reader.BaseStream.Seek(start_offset, SeekOrigin.Begin);
+                    }
+                }
             }
         }
     }
